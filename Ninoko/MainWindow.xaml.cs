a25e0895d1543feadb47a019597eefe3828662de
Arina Bezborodova <arina.bezborodova@yandex.ru>
2019-12-14 14:26:44 +0300

    Добавьте файлы проекта.

---
+++ Ninoko/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ninoko
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string LoginBox { get; set; }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            //if (loginBox.Text.ToString().In(SmartParkingApp.users.ToString()) & passwordBox.Text.ToString().In(SmartParkingApp.users.ToString()))
+            //{
+            //    LoginBox = loginBox.Text.ToString();
+            //    var window = new ClientPageWindow(LoginBox);
+            //    window.ShowDialog();
+            //}
+
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
